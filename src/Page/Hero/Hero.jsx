d33806037bcb4380f7b62ec4c8b6b3f2a8f79e8b
marketLube <marketlubes@gmail.com>
2025-02-08 16:02:33 +0530

    sl

--- src/Page/Hero/Hero.jsx
+++ src/Page/Hero/Hero.jsx
@@ -1,7 +1,17 @@
+import logo from "../../assets/logo/logo.png";
+
 export default function Hero() {
   return (
     <div className="hero">
       <div className="hero-header"></div>
+      <div className="hero-content">
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="hero-content-text">
+          BUILD THE FOUNDATION OF BETTER TOMMARROW
+        </div>
+      </div>
     </div>
   );
 }
