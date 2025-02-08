76cd9a520c9f61534bbc830bcf5e73d4f76ffae1
marketLube <marketlubes@gmail.com>
2025-02-08 13:06:32 +0530

    sl

---
+++ src/Page/Section3/Stats.jsx
@@ -0,0 +1,16 @@
+export default function Stats() {
+  return (
+    <div className="stats">
+      <div className="stats-img-container"></div>
+      <div className="stats-content">
+        <div className="stats-content-header"></div>
+        <div className="stats-content-body">
+          <div className="stats-content-body-item"></div>
+          <div className="stats-content-body-item"></div>
+          <div className="stats-content-body-item"></div>
+          <div className="stats-content-body-item"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
