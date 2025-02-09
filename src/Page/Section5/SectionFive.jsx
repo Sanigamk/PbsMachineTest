46ba053551524c3990e37ef7a4aa835d7d532c09
marketLube <marketlubes@gmail.com>
2025-02-09 17:29:00 +0530

    buttons

--- src/Page/Section5/SectionFive.jsx
+++ src/Page/Section5/SectionFive.jsx
@@ -14,8 +14,23 @@ export default function SectionFive() {
       </div>
       <div className="section-five-content">
         <div className="section-five-content-top">
-          <button className="about-content-read-more">
-            READ MORE <span>&rarr;</span>
+          <button className="footer-header-button">
+            CONTACT US{" "}
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="12"
+                height="12.029"
+                viewBox="0 0 12 12.029"
+              >
+                <path
+                  id="arrow-up-right"
+                  d="M12,95.855v8.374a.6.6,0,1,1-1.2,0V97.306l-9.776,9.776a.6.6,0,1,1-.848-.848l9.784-9.784L3,96.43a.6.6,0,0,1,0-1.2l8.4.025A.6.6,0,0,1,12,95.855Z"
+                  transform="translate(-0.003 -95.23)"
+                  fill="#dfa01e"
+                />
+              </svg>
+            </span>
           </button>
         </div>
         <div className="section-five-content-body">
